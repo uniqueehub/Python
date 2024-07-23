5c253c663dcf03449474ecaff2101f741a0967a3
NIKITA RAJENDRA  BHAWAR <125591201+uniqueehub@users.noreply.github.com>
2024-07-23 13:43:47 +0530

    Add files via upload
    Numbers

---
+++ Numbers.py
@@ -0,0 +1,32 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 6+7
+13
+>>> 8/5
+1.6
+>>> 2^45
+47
+>>> 44*89
+3916
+>>> a=6
+>>> b=7
+>>> c=a+b
+>>> print(c)
+13
+>>> 10+2*3
+16
+>>> (10+2)*3
+36
+>>> c/45.6
+0.2850877192982456
+>>> round(time, 2)
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    round(time, 2)
+NameError: name 'time' is not defined. Did you forget to import 'time'?
+>>> round(c/45.6, 2)
+0.29
+>>> 6-5.7
+0.2999999999999998
+>>> round(6-5.7,2)
+0.3
