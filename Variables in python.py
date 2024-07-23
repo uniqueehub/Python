59ce8867cadfe3fdfbeff4cd0ca3d4b5d2b3d229
NIKITA RAJENDRA  BHAWAR <125591201+uniqueehub@users.noreply.github.com>
2024-07-23 10:16:36 +0530

    Add files via upload
    Variables

---
+++ Variables in python.py
@@ -0,0 +1,16 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #claculate expenses
+>>> rent=300
+>>> gas=600
+>>> maggie=90
+>>> print(rent)
+300
+>>> total=rent+gas+maggie
+>>> print(total)
+990
+>>> item1="rent"
+>>> item2="gas"
+>>> items3="maggie"
+>>> print("Expenses are in:", item1,item2,items3)
+Expenses are in: rent gas maggie
