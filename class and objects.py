a096d57df03d8506104ec87d2728e172ec8fc90c
uniqueehub <bhawarsakshi73@email.com>
2024-08-18 17:20:04 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ class and objects.py
@@ -0,0 +1,12 @@
+class nikita:
+    x=5
+p1=nikita()
+print(nikita.x)
+
+class person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+p1=person("Nikita",22)
+print(p1.name)
+print(p1.age)
